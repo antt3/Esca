2f6cc310e493a790bc050d8b232e7354ae3aba16
Anthony T <anthonyt3@sbcglobal.net>
2022-06-01 22:34:01 -0700

    ++ PostReducer

---
+++ auth-me/frontend/src/store/postsReducer.js
@@ -0,0 +1,51 @@
+const LOAD_POSTS = 'articles/loadPosts';
+const ADD_POST = 'articles/addPosts';
+
+export const loadPosts = (posts) => {
+  return {
+    type: LOAD_POSTS,
+    posts
+  };
+};
+
+export const addPosts = (posts) => {
+  return {
+    type: ADD_POST,
+    posts
+  };
+};
+
+export const fetchPosts = () => async (dispatch) => {
+  const response = await fetch('/api/posts');
+  const posts = await response.json();
+  dispatch(loadPosts(posts));
+};
+
+export const writePost = (payload) => async (dispatch) => {
+  const response = await fetch('/api/posts', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
+  });
+
+  if (response.ok) {
+    const post = await response.json();
+    dispatch((post));
+    return post;
+  }
+};
+
+const initialState = { entries: {}, isLoading: true };
+
+const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case LOAD_POSTS: 
+      return { ...state, entries: [...action.posts] };
+    case ADD_POST:
+      return { ...state, entries: [...state.entries, action.posts] };
+    default:
+      return state;
+  }
+};
+
+export default postReducer;
